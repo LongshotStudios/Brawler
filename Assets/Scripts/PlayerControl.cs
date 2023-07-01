e6c15aa8cab58730949c3dde45a827d2e24428b8
Nathan Fabian <ndfabian@gmail.com>
2023-06-30 22:51:52 -0600

    knight character with more actions

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -8,16 +8,26 @@ public class PlayerControl : MonoBehaviour
     public float walkSpeed = 1.0f;
     private Vector2 lastInput;
     private Animator animator;
+    
+    public Transform animatedChild;
+    private Vector3 originalScale;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        originalScale = animatedChild.localScale;
     }
     private void Update()
     {
         animator.SetFloat("Speed", lastInput.magnitude);
         var dist = walkSpeed * Time.deltaTime;
         transform.position += new Vector3(lastInput.x * dist, 0, lastInput.y * dist);
+
+        if (lastInput.x != 0) { 
+            var set = originalScale;
+            set.x *= Mathf.Sign(lastInput.x);
+            animatedChild.localScale = set;
+        }
     }
     
     private void OnMovement(InputValue value)
