cc1bad997b4f84a8a45d32f3adabfb1fb0955b68
Nathan Fabian <ndfabian@gmail.com>
2023-07-23 11:28:18 -0700

    Some updated images and path colliders for the background

--- Assets/Scripts/PlayerControl2D.cs
+++ Assets/Scripts/PlayerControl2D.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,24 +8,30 @@ public class PlayerControl2D : MonoBehaviour
 {
     public float walkSpeed = 1.0f;
     private Vector2 lastInput;
+    private Rigidbody2D rb;
     private Animator animator;
     private SpriteRenderer spriteRenderer;
 
     private void Awake()
     {
+        rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
+
     private void Update()
     {
         animator.SetFloat("Speed", lastInput.magnitude);
-        var dist = walkSpeed * Time.deltaTime;
-        transform.position += new Vector3(lastInput.x * dist, lastInput.y * dist, 0);
         if (lastInput.x != 0) {
             spriteRenderer.flipX = lastInput.x < 0;
         }
-    }
+    } 
     
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + lastInput * walkSpeed * Time.fixedDeltaTime);
+    }
+
     private void OnMovement(InputValue value)
     {
        lastInput = value.Get<Vector2>();
