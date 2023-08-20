1d6a35abad31f289f5eb068e642a31083d215fab
Nathan Fabian <ndfabian@gmail.com>
2023-08-20 16:28:03 -0600

    Camera follow, need to work on interaction layers.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Vector2 scrollMin;
+    public Vector2 scrollMax;
+    public Transform followTarget;
+    public float followSpeed;
+    private Camera cam;
+
+    private void Awake()
+    {
+        cam = GetComponent<Camera>();
+    }
+    
+
+    private void Update()
+    {
+        var screenTarget = cam.WorldToViewportPoint(followTarget.position);
+        var move = screenTarget - new Vector3(0.5f, 0.5f, screenTarget.z);
+        if (move.x > 0.25f) {
+            move.x = 1;
+        } else if (move.x < -.25f) {
+            move.x = -1;
+        } else {
+            move.x = 0;
+        }
+        var pos = transform.position + move * followSpeed * Time.deltaTime;
+        pos.x = Mathf.Clamp(pos.x, scrollMin.x, scrollMax.x);
+        pos.y = Mathf.Clamp(pos.y, scrollMin.y, scrollMax.y);
+        transform.position = pos;
+    }
+}
