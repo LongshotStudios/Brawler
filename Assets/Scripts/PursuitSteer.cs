e0c8a065ce21cd4e0b8d44f6842bd4d2ffc07523
Nathan Fabian <ndfabian@gmail.com>
2023-09-18 07:56:54 -0600

    Better stopping behavior

--- Assets/Scripts/PursuitSteer.cs
+++ Assets/Scripts/PursuitSteer.cs
@@ -8,6 +8,7 @@ public class PursuitSteer : ActionTask
     public BBParameter<float> maxSpeed;
     public BBParameter<Vector2> inTargetPosition;
     public BBParameter<Vector2> inTargetVelocity;
+    public BBParameter<bool> inFlipped;
     public BBParameter<Vector2> outTargetSteer;
 
     protected override void OnUpdate()
@@ -15,16 +16,19 @@ public class PursuitSteer : ActionTask
         // estimate pursuit position
         var dir = inTargetPosition.value - (Vector2)agent.transform.position;
         var dis = dir.magnitude;
-        if (dis < 0.1f)
-        {
-            EndAction(false);
-            return;
+        if (dis < Mathf.Epsilon) {
+            if (inFlipped.value == agent.GetComponent<SpriteRenderer>().flipX) {
+                outTargetSteer.value = Vector2.zero;
+            } else {
+                float x = inFlipped.value ? -0.1f : 0.1f;
+                outTargetSteer.value = new Vector2(x, 0);
+            }
+        } else {
+            dir /= dis;
+            var maxDist = maxSpeed.value * Time.fixedDeltaTime;
+            var steerRatio = Mathf.Clamp(dis / maxDist, 0f, 1f);
+            outTargetSteer.value = steerRatio * dir;
         }
-        dir /= dis;
-        var maxDist = maxSpeed.value * Time.fixedDeltaTime;
-        var steerRatio = Mathf.Clamp(dis / maxDist, 0f, 1f);
-        Debug.Log("SteerRatio " + steerRatio + " dir " + dir);
-        outTargetSteer.value = steerRatio * dir;
         EndAction(true);
     }
 }
--- Assets/Scripts/SensePlayer.cs
+++ Assets/Scripts/SensePlayer.cs
@@ -9,12 +9,14 @@ public class SensePlayer : ActionTask
     public BBParameter<List<Vector2>> inAttackOffsets;
     public BBParameter<bool> outHasTarget;
     public BBParameter<Vector2> outTargetPosition;
-    public BBParameter<Vector2> outTargetVelocity; 
+    public BBParameter<Vector2> outTargetVelocity;
+    public BBParameter<bool> outFlipped;
     
     protected override void OnUpdate()
     {
         Vector2 target = Vector2.zero;
         float targetDistance = Mathf.Infinity;
+        bool flipped = false;
         
         var colliders = Physics2D.OverlapCircleAll(agent.transform.position, inSenseRadius.value);
         foreach (var collider in colliders) {
@@ -31,6 +33,7 @@ public class SensePlayer : ActionTask
                 {
                     target = targetCandidate;
                     targetDistance = dist;
+                    flipped = collider.transform.position.x < agent.transform.position.x;
                 }
             }
         }
@@ -39,6 +42,7 @@ public class SensePlayer : ActionTask
             outHasTarget.value = true;
             outTargetPosition.value = target;
             outTargetVelocity.value = Vector2.zero;
+            outFlipped.value = flipped;
             EndAction(true);
         } else {
             outHasTarget.value = false;
